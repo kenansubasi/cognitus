5efebe65b68c56d6d7df8ba61a05a22439522a09
Kenan Subaşı <kenansubasiceng@gmail.com>
2020-04-01 00:51:24 +0300

    refs #11: Algorithm prediction endpoint has been added.

--- algorithm/app.py
+++ algorithm/app.py
@@ -1,13 +1,15 @@
 import os
 
-from flask import Flask, make_response
+from flask import Flask, jsonify, make_response, request
 from flask_sqlalchemy import SQLAlchemy
 
 from algorithm.celery_app import train_task
+from algorithm.helpers import load_model
 
 # App
 app = Flask(__name__)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+RESULTS_PATH = os.environ.get("RESULTS_PATH", "/code/results/")
 
 # DB Config
 POSTGRES_USER = os.environ.get("POSTGRES_USER")
@@ -22,7 +24,7 @@ db = SQLAlchemy(app)
 Data = db.Table("data_data", db.metadata, autoload=True, autoload_with=db.engine)
 
 
-@app.route('/train/')
+@app.route("/train/", methods=["GET"])
 def train():
     data = db.session.query(Data).all()
     text_list = []
@@ -33,12 +35,44 @@ def train():
         label_list.append(item.label)
 
     data_count = len(text_list)
-    data = {
+    response = {
         "count": data_count
     }
 
     if data_count > 0:
         train_task.delay(text_list, label_list)
-        return make_response(data, 200)
+        return make_response(response, 200)
     else:
-        return make_response(data, 400)
+        return make_response(response, 400)
+
+
+@app.route("/prediction/", methods=["POST"])
+def prediction():
+    request_data = request.get_json()
+
+    text = request_data.get("text")
+    if not text:
+        response = {
+            "text": {
+                "required": "This field is required!"
+            }
+        }
+        return make_response(response, 400)
+
+    model_file = RESULTS_PATH + "model.pickle"
+    vectorizer_file = RESULTS_PATH + "vectorizer.pickle"
+    if not os.path.exists(model_file) or  not os.path.exists(vectorizer_file):
+        response = {
+            "non_field_errors": [
+                "Model and Vectorizer data not found. Please train first."
+            ]
+        }
+        return make_response(response, 400)
+
+    model = load_model(model_file)
+    vectorizer = load_model(vectorizer_file)
+    result = model.predict(vectorizer.transform([text]))
+    response = {
+        "result": result.tolist()
+    }
+    return make_response(jsonify(response), 200)
--- algorithm/celery_app.py
+++ algorithm/celery_app.py
@@ -8,7 +8,7 @@ from algorithm.helpers import tfidf, test_SVM, dump_model
 # Celery Config
 CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL")
 CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND")
-RESULT_PATH = os.environ.get("RESULT_PATH", "/code/results/")
+RESULTS_PATH = os.environ.get("RESULTS_PATH", "/code/results/")
 celery_app = Celery("algorithm", broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND)
 
 
@@ -18,9 +18,9 @@ def train_task(text, label):
     x_train, x_test, y_train, y_test = cross_validation.train_test_split(training, label, test_size=0.25,
                                                                          random_state=0)
     model, accuracy, precision, recall = test_SVM(x_train, x_test, y_train, y_test)
-    if not os.path.exists(RESULT_PATH):
-        os.makedirs(RESULT_PATH)
-    dump_model(model, RESULT_PATH + "model.pickle")
-    dump_model(vectorizer, RESULT_PATH + "vectorizer.pickle")
+    if not os.path.exists(RESULTS_PATH):
+        os.makedirs(RESULTS_PATH)
+    dump_model(model, RESULTS_PATH + "model.pickle")
+    dump_model(vectorizer, RESULTS_PATH + "vectorizer.pickle")
 
     return "Success"

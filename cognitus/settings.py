8d44b43be371230a18a7e40e072cc1c6f1ffa589
Kenan Subaşı <kenansubasiceng@gmail.com>
2020-04-02 04:29:14 +0300

    refs #14: Pagination has been added.

--- cognitus/settings.py
+++ cognitus/settings.py
@@ -155,3 +155,11 @@ FIXTURE_DIRS = (
 SWAGGER_SETTINGS = {
     "DEFAULT_INFO": "cognitus.urls.api_info",
 }
+
+
+# Rest Framework
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100
+}

a82ee457d216830e3443b2a6a0384fd8d8204012
Kenan Subaşı <kenansubasiceng@gmail.com>
2020-04-05 00:40:04 +0300

    refs #17: retry added to train_task.

--- algorithm/celery_app.py
+++ algorithm/celery_app.py
@@ -11,15 +11,16 @@ from conf.constants import RESULTS_PATH, CELERY_BROKER_URL, CELERY_RESULT_BACKEN
 celery_app = Celery("algorithm", broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND)
 
 
-@celery_app.task()
-def train_task(text, label):
-    training, vectorizer = tfidf(text)
-    x_train, x_test, y_train, y_test = cross_validation.train_test_split(training, label, test_size=0.25,
-                                                                         random_state=0)
-    model, accuracy, precision, recall = test_SVM(x_train, x_test, y_train, y_test)
-    if not os.path.exists(RESULTS_PATH):
-        os.makedirs(RESULTS_PATH)
-    dump_model(model, RESULTS_PATH + "model.pickle")
-    dump_model(vectorizer, RESULTS_PATH + "vectorizer.pickle")
-
-    return "Success"
+@celery_app.task(name="algorithm.train_task", bind=True, max_retries=3)
+def train_task(self, text, label):
+    try:
+        training, vectorizer = tfidf(text)
+        x_train, x_test, y_train, y_test = cross_validation.train_test_split(training, label, test_size=0.25,
+                                                                             random_state=0)
+        model, accuracy, precision, recall = test_SVM(x_train, x_test, y_train, y_test)
+        if not os.path.exists(RESULTS_PATH):
+            os.makedirs(RESULTS_PATH)
+        dump_model(model, RESULTS_PATH + "model.pickle")
+        dump_model(vectorizer, RESULTS_PATH + "vectorizer.pickle")
+    except Exception as err:
+        self.retry(countdown=3 ** self.request.retries)
